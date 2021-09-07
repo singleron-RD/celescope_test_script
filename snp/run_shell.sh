2b5fb7df146f3e9c53ac5101880baf58ab0a74e5
aironi <83407205+lzh233@users.noreply.github.com>
2021-09-07 14:29:19 +0800

    Update run_shell.sh

--- snp/run_shell.sh
+++ snp/run_shell.sh
@@ -2,6 +2,4 @@ multi_snp --mapfile ./snp.mapfile \
           --gene_list ../celescope_test_data/gene_list.tsv \
           --genomeDir /SGRNJ03/randd/public/genome/rna/hs_ensembl_99 \
           --annovar_config ./annovar.config \
-          --mod shell \
-          #--overlap 8 \
-          #--not_consensus 
+          --mod shell
