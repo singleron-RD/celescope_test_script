db7053f1885dd0a9006cdfc5685e780bf592c31d
aironi <83407205+lzh233@users.noreply.github.com>
2021-09-09 15:20:40 +0800

    change the test data path in  run_shell.sh and snp.mapfile (#2)
    * snp test script
    
    * Update run_shell.sh
    
    * change path of test data

--- snp/run_shell.sh
+++ snp/run_shell.sh
@@ -1,5 +1,5 @@
 multi_snp --mapfile ./snp.mapfile \
-          --gene_list ../celescope_test_data/gene_list.tsv \
+          --gene_list ../../celescope_test_data/snp/gene_list.tsv \
           --genomeDir /SGRNJ03/randd/public/genome/rna/hs_ensembl_99 \
           --annovar_config ./annovar.config \
           --mod shell
