9254c4d611ec91858599393fea0223d9538b7ba5
zhouyiqi <zhouyiqi@singleronbio.com>
2021-09-17 15:57:52 +0800

    update

--- snp/run_shell.sh
+++ snp/run_shell.sh
@@ -1,5 +1,7 @@
-multi_snp --mapfile ./snp.mapfile \
-          --gene_list ../../celescope_test_data/snp/gene_list.tsv \
-          --genomeDir /SGRNJ03/randd/public/genome/rna/hs_ensembl_99 \
-          --annovar_config ./annovar.config \
-          --mod shell
+multi_snp \
+ --mapfile ./snp.mapfile \
+ --gene_list ../../celescope_test_data/snp/gene_list.tsv \
+ --genomeDir /SGRNJ03/randd/public/genome/rna/hs_ensembl_99 \
+ --annovar_config ./annovar.config \
+ --mod shell\
+ --not_consensus
