8f3a0e4e7ea56ae9d93f2f4c409f826fcbf27ccf
aironi <83407205+lzh233@users.noreply.github.com>
2021-12-24 14:39:24 +0800

    add virus test script (#3)
    * add virus test script
    
    * add virus test script
    
    * add virus test script

---
+++ virus/run_shell.sh
@@ -0,0 +1,10 @@
+multi_capture_virus\
+ --mapfile ./virus.mapfile\
+ --chemistry scopeV2.2.1\
+ --virus_genomeDir ../../celescope_test_data/virus/EBV_genome \
+ --thread 4\
+ --outFilterMatchNmin 80 \
+ --not_consensus \
+ --umi_threshold auto \
+ --allowNoPolyT \
+ --mod shell
