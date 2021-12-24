48553cffcd89d96fb3bd5eb62f182e033162390c
aironi <360370285@qq.com>
2021-12-24 14:08:00 +0800

    add virus test script

---
+++ virus/run_shell.sh
@@ -0,0 +1,10 @@
+multi_capture_virus\
+ --mapfile ./virus.mapfile\
+ --chemistry scopeV2.2.1\
+ --virus_genomeDir /SGRNJ03/randd/user/liuzihao/work/t1/test_data/EBV_genome \
+ --thread 4\
+ --outFilterMatchNmin 80 \
+ --not_consensus \
+ --umi_threshold auto \
+ --allowNoPolyT \
+ --mod shell
