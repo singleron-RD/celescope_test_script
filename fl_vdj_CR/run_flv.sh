f9295763e8f067cf9c8b42169a658c31a4491f25
Chenjunjie1996 <74704889+Chenjunjie1996@users.noreply.github.com>
2022-04-19 15:24:58 +0800

    fl vdj test script (#8)
    * full len vdj
    
    * fl vdj test script
    
    Co-authored-by: 陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>
    Co-authored-by: 陈俊杰 <chenjunjie@SGRNJlogin.sgr.com>

---
+++ fl_vdj_CR/run_flv.sh
@@ -0,0 +1,11 @@
+multi_fl_vdj_CR \
+	--mapfile  ./test.mapfile \
+	--chemistry flv \
+	--mem 10 \
+	--species hs \
+	--thread 8 \
+	--allowNoLinker \
+	--seqtype TCR \
+	--ref_path "/SGRNJ/Database/script/soft/cellranger/vdj_ref/6.0.0/hs/refdata-cellranger-vdj-GRCh38-alts-ensembl-5.0.0" \
+	--soft_path "/SGRNJ03/randd/cjj/soft/cellranger/cellranger-6.1.2/cellranger" \
+	--mod shell 
---
+++ fl_vdj_TRUST4/run_flv.sh
@@ -0,0 +1,9 @@
+multi_fl_vdj_TRUST4 \
+	--mapfile  ./test.mapfile \
+	--chemistry flv \
+	--species hg38 \
+	--thread 10 \
+	--allowNoLinker \
+	--seqtype TCR \
+	--match_previous_assemble \
+	--mod shell
