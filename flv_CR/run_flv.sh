50852a876258ae5f2bd692ada4eade0b9b0a8628
zhouyiqi <zhouyiqi@singleronbio.com>
2022-06-14 14:41:06 +0800

    TODO fix summarize keyerror

--- flv_CR/run_flv.sh
+++ flv_CR/run_flv.sh
@@ -1,9 +1,5 @@
 multi_flv_CR \
-	--mapfile  ./test.mapfile\
-	--mem 10 \
-	--species hs \
-	--thread 8 \
-	--allowNoLinker \
+	--mapfile  ./test.mapfile \
 	--seqtype TCR \
 	--ref_path "/SGRNJ/Database/script/soft/cellranger/vdj_ref/6.0.0/hs/refdata-cellranger-vdj-GRCh38-alts-ensembl-5.0.0" \
 	--soft_path "/SGRNJ03/randd/cjj/soft/cellranger/cellranger-6.1.2/cellranger" \
