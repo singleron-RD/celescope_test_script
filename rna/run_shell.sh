a1b17852f9c58ca41a541abfdecb4ff704aaaf7a
zhouyiqi <zhouyiqi@singleronbio.com>
2022-09-14 17:04:54 +0800

    filter gtf

--- rna/run_shell.sh
+++ rna/run_shell.sh
@@ -1,6 +1,6 @@
 multi_rna\
  --mapfile ./rna.mapfile\
- --genomeDir /SGRNJ03/randd/public/genome/rna/hs_ensembl_99\
+ --genomeDir /SGRNJ06/randd/USER/zhouyiqi/test/gtf_filter\
  --thread 4\
  --mod shell
  #--steps_run analysis
