3af998d4c19ee3bba4203add1e23ff579ed76aaf
zhouyiqi <zhouyiqi@singleronbio.com>
2022-10-28 11:07:59 +0800

    v1.13.0

--- capture_virus/old.sh
+++ capture_virus/old.sh
@@ -3,5 +3,4 @@ multi_capture_virus\
  --virus_genomeDir ../../celescope_test_data/capture_virus/EBV_genome \
  --not_consensus \
  --mod shell\
- --gtf ../../celescope_test_data/capture_virus/EBV_genome/EBV_five_genes.gtf\
- 
+
--- sweetseq/run_shell.sh
+++ sweetseq/run_shell.sh
@@ -1,6 +1,6 @@
 multi_sweetseq \
  --mapfile ./sweetseq.mapfile \
  --linker_fasta ../../celescope_test_data/sweetseq/sweet_tag_linker.fasta \
- --tag_barcode_fasta ../../celescope_test_data/sweetseq/sweet_tag_barcode.fasta \
+ --barcode_fasta ../../celescope_test_data/sweetseq/sweet_tag_barcode.fasta \
  --fq_pattern L23C15 \
  --mod shell\
