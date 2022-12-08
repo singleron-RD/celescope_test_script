d54727ef85d3005c8470740d253ff17ebad54791
Chenjunjie1996 <74704889+Chenjunjie1996@users.noreply.github.com>
2022-12-08 17:50:17 +0800

    re-write vdj and snp test script (#14)
    * fl_vdj
    
    * re-write vdj and snp test script
    
    Co-authored-by: 陈俊杰 <chenjunjie@SGRNJshiny.sgr.com>

--- snp/run_shell.sh
+++ snp/run_shell.sh
@@ -1,7 +1,6 @@
 multi_snp \
  --mapfile ./snp.mapfile \
  --genomeDir /SGRNJ03/randd/public/genome/rna/hs_ensembl_99 \
- --annovar_config ./annovar.config \
  --mod shell\
  --not_consensus\
  --panel lung_1
--- vdj/run_shell.sh
+++ vdj/run_shell.sh
@@ -1,7 +1,8 @@
-multi_vdj\
- --mapfile ./tcr.mapfile\
+multi_vdj \
+ --mapfile ./tcr.mapfile \
+ --ref_path /SGRNJ06/randd/USER/cjj/igblast/igblast_ref/hs_TR \
  --type TCR\
  --mod shell\
- --target_cell_barcode target_cell_barcode.txt\
+ #--target_cell_barcode target_cell_barcode.txt\
  
 
